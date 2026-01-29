bed900c863ba1f6a928b292ab3f1b3fe238ffdf2
abheeshakespeare <kumarabhishekmishra28@gmail.com>
2026-01-29 22:35:01 +0530

    Number Spiral

---
+++ IntroductoryProblems/Number_Spiral.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+
+int main() {
+    fastio();
+    int n;
+    cout<<n;
+    return 0;
+}
